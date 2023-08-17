42abac2a493551acda57a9abba3267d12f7af08c
Wagner Rodrigues <wagnersr92@gmail.com>
2023-08-17 01:30:27 -0300

    Initial Commit

---
+++ KingBurguer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurguer
+//
+//  Created by Wagner Rodrigues on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
